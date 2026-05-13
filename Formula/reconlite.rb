fcb51be0dcc81f5091d84dc685b0e0317dfb61e5
DEBASIS <185465407+DebaA17@users.noreply.github.com>
2026-05-13 22:53:04 +0530

    enhanced search handling for reconlite

--- Formula/reconlite.rb
+++ Formula/reconlite.rb
@@ -9,11 +9,6 @@ class Reconlite < Formula
 
   depends_on "python@3.13"
 
-  depends_on "pkg-config" => :build
-  depends_on "libxml2"
-  depends_on "libxslt"
-  depends_on "rust" => :build
-
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/25/ee/6caf7a40c36a1220410afe15a1cc64993a1f864871f698c0f93acb72842a/certifi-2026.4.22.tar.gz"
     sha256 "8d455352a37b71bf76a79caa83a3d6c25afee4a385d632127b6afb3963f1c580"
@@ -22,16 +17,6 @@ class Reconlite < Formula
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
     sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
-  end
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/bb/63/f9e1ea081ce35720d8b92acde70daaedace594dc93b693c869e0d5910718/click-8.3.3.tar.gz"
-    sha256 "398329ad4837b2ff7cbe1dd166a4c0f8900c3ca3a218de04466f38f6497f18a2"
-  end
-
-  resource "ddgs" do
-    url "https://files.pythonhosted.org/packages/e6/31/4b8ad86fd97fba7cff52d9d7c59a002ddf9ef0ba8fa4d70b925190471c33/ddgs-9.14.2.tar.gz"
-    sha256 "a9e6ad5bd7357707163d1cf03dbbcc9413a5820738ba5176efe36955b32aab38"
   end
 
   resource "defusedxml" do
@@ -52,16 +37,6 @@ class Reconlite < Formula
   resource "ipwhois" do
     url "https://files.pythonhosted.org/packages/ed/e9/f1dd533366b5809494d0ee0d39db2e0806388c52c25b1dcff2e0794a51d8/ipwhois-1.3.0.tar.gz"
     sha256 "11d59784b6350210c33e12301cc50cd1ebfbb0b12a7b0f97916e11d6661c4d5d"
-  end
-
-  resource "lxml" do
-    url "https://files.pythonhosted.org/packages/28/30/9abc9e34c657c33834eaf6cd02124c61bdf5944d802aa48e69be8da3585d/lxml-6.1.0.tar.gz"
-    sha256 "bfd57d8008c4965709a919c3e9a98f76c2c7cb319086b3d26858250620023b13"
-  end
-
-  resource "primp" do
-    url "https://files.pythonhosted.org/packages/ee/ca/383bdf0df3dc87b60bf73c55da526ac743d42c5155a84a9014775b895e96/primp-1.2.3.tar.gz"
-    sha256 "a531b01f57cb59e3e7a3a2b526bb151b61dc7b2e15d2f6961615a553632e2889"
   end
 
   resource "python-dateutil" do
@@ -90,6 +65,11 @@ class Reconlite < Formula
   end
 
   def install
+    inreplace "reconlite.py", "    from ddgs import DDGS\n", ""
+    inreplace "reconlite.py",
+              "        try:\n            # Use DuckDuckGo to search for subdomains\n",
+              "        if \"DDGS\" not in globals():\n            return []\n\n        try:\n            # Use DuckDuckGo to search for subdomains\n"
+
     venv = virtualenv_create(libexec, "python3.13")
     venv.pip_install resources
 
