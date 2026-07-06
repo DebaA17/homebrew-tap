74e163179ff3d2ad058752956652fcdff572345e
DEBASIS <185465407+DebaA17@users.noreply.github.com>
2026-07-06 12:25:55 +0530

    Update cvecli to v1.2.2

--- Formula/cvecli.rb
+++ Formula/cvecli.rb
@@ -1,17 +1,17 @@
 class Cvecli < Formula
   desc "Command-line tool to search CVEs using public APIs"
   homepage "https://github.com/DebaA17/CVE-scanner-cli"
-  version "1.2.1"
+  version "1.2.2"
   license "MIT"
 
   on_macos do
-    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.1/cvecli-1.2.1-macos.zip"
-    sha256 "f616bc4a9a2447c6ebfb07f3362e0ef2153f2e98841d3311ad23fb85d672efdc"
+    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.2/cvecli-1.2.2-macos.zip"
+    sha256 "a03b256eae53fb833e835d03e1dab3603ff87da7364839a9aaa83e49012d63b8"
   end
 
   on_linux do
-    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.1/cvecli-1.2.1-linux.zip"
-    sha256 "c8d1cbe980b713bb19158bdc1f9ec0ae5b1b980c7d6ac1408a9bd49a046e92d0"
+    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.2/cvecli-1.2.2-linux.zip"
+    sha256 "c3cf53653ef1b365a095684ad823905325bd2ff0ec2a9039fe1294b77eb7ce85"
   end
 
   def install
