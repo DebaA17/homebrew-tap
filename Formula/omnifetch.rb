8e687e299931ad319e7f3269cd98242734e45d34
DEBASIS <185465407+DebaA17@users.noreply.github.com>
2026-07-23 13:38:44 +0530

    add omnifetch latest version

--- Formula/omnifetch.rb
+++ Formula/omnifetch.rb
@@ -1,8 +1,8 @@
 class Omnifetch < Formula
   desc "Downloader for public files, articles, and media"
   homepage "https://github.com/DebaA17/omnifetch"
-  url "https://github.com/DebaA17/omnifetch/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "4a0ddb3a3eccf73b17585c6b3e71d1da2343621c7dee674407164963547126e6"
+  url "https://github.com/DebaA17/omnifetch/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "051029e5bbdc60a9b851b2e1cf2322c1f012e43172e2af4a189fc25b9e774402"
   license "MIT"
 
   depends_on "go" => :build
