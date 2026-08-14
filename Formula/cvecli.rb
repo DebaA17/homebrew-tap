dfa3d87467db724d6e6a633a9379ff7080175d21
DEBASIS BISWAS <185465407+DebaA17@users.noreply.github.com>
2026-08-14 12:14:21 +0000

    Update cvecli to version 1.3.0 for macOS and Linux

--- Formula/cvecli.rb
+++ Formula/cvecli.rb
@@ -4,11 +4,11 @@ class Cvecli < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.2/cvecli-1.2.2-macos.zip"
-    sha256 "a03b256eae53fb833e835d03e1dab3603ff87da7364839a9aaa83e49012d63b8"
+    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.3.0/cvecli-1.3.0-linux.zip"
+    sha256 "sha256:7d877adc355d324706d61ec57d06a5b5f7a9be5f1cd6bfc12f45874a2748dd98"
   else
-    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.2.2/cvecli-1.2.2-linux.zip"
-    sha256 "c3cf53653ef1b365a095684ad823905325bd2ff0ec2a9039fe1294b77eb7ce85"
+    url "https://github.com/DebaA17/CVE-scanner-cli/releases/download/v1.3.0/cvecli-1.3.0-macos.zip"
+    sha256 "sha256:ec322ae4882806b1c6903de0a85aaaa601f809fb33a64f90a59cdefb754daa1b"
   end
 
   def install
